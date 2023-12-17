ffe4e643325b5dc8bd8d6acead088e19cf7f5c36
Jaleesa Robinsin <robinson.jaleesa42@gmail.com>
2023-12-17 15:55:46 -0500

    correted homework

--- index.js
+++ index.js
@@ -1,45 +1,40 @@
 const myMap = {
-    coordintates: [39.75033, -104.99185, 39.74963, -104.99245,39.74891, -104.99111]
-    businesses: [39.74893, -104.99163], 
-    map: {},
-    markers: {},
-}
+	coordinates: [],
+	businesses: [],
+	map: {},
+	markers: {},
 
- buildupMap() {
-    this.map = L.map('map', {
-      center: [39.75033, -104.99185],
-      zoom: 12,  
-    });
+	
+	buildMap() {
+		this.map = L.map('map', {
+		center: this.coordinates,
+		zoom: 11,
+		});
+	
+		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution:
+			'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+		minZoom: '15',
+		}).addTo(this.map)
+	
+		const marker = L.marker(this.coordinates)
+		marker
+		.addTo(this.map)
+		.bindPopup('<p1><b>You are here</b><br></p1>')
+		.openPopup()
+	},
 
-    
- 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    minZoom: '15',
-}).addTo(this.map)
 
-const marker = L.marker([39.74963, -104.99245],[39.74893, -104.99163], [39.74891, -104.99111], [39.74844, -104.99084],)
-marker.addTo (this,map)
-.bindPopup('p1><b>You Should Be Here</b><p1>')
-.openPopup()
-
-// const polygon = L.polygon([
-//     [39.74963, -104.99245],
-//     [39.74893, -104.99163],
-//     [39.74891, -104.99111],
-//     [39.74844, -104.99084],
-//     []
-// ]).addTo(myMap)
-
-addMarkers() {
-    for (var i = 0; i < this.businesses.length; i++) {
-    this.markers = L.marker([
-        this.businesses[i].lat,
-        this.businesses[i].long,
-    ])
-        .bindPopup(`<p1>${this.businesses[i].name}</p1>`)
-        .addTo(this.map)
-    }
+	addMarkers() {
+		for (var i = 0; i < this.businesses.length; i++) {
+		this.markers = L.marker([
+			this.businesses[i].lat,
+			this.businesses[i].long,
+		])
+			.bindPopup(`<p1>${this.businesses[i].name}</p1>`)
+			.addTo(this.map)
+		}
+	},
 }
 
 
@@ -47,25 +42,26 @@ async function getCoords(){
 	const pos = await new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(resolve, reject)
 	});
+	console.log(myMap)
 	return [pos.coords.latitude, pos.coords.longitude]
 }
 
 async function getFoursquare(business) {
-    const options = {
-        method: 'GET',
-        headers: {
-            Accept: 'application/json'
-            Authorization: 
-        }
-    }
-
-    let limit = 5
+	const options = {
+		method: 'GET',
+		headers: {
+		Accept: 'application/json',
+		Authorization: 'fsq3pfcQ088rfvNwnZgmuKAxdFNqEBgOD+7KZbluARqVRco='
+		}
+	}
+	let limit = 5
 	let lat = myMap.coordinates[0]
 	let lon = myMap.coordinates[1]
-	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let response = await fetch(`https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
 	let data = await response.text()
 	let parsedData = JSON.parse(data)
 	let businesses = parsedData.results
+	console.log(business)
 	return businesses
 }
 
@@ -78,14 +74,18 @@ function processBusinesses(data) {
 		};
 		return location
 	})
+	console.log(businesses)
 	return businesses
 }
+
+
 
 window.onload = async () => {
 	const coords = await getCoords()
 	myMap.coordinates = coords
-	myMap.buildMap(Markets)
+	myMap.buildMap()
 }
+
 
 document.getElementById('submit').addEventListener('click', async (event) => {
 	event.preventDefault()
